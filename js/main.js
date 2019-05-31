a785d7d1ded88c2ade8d769e3febe7d90769c7be
Pradeep Talwar <ptalwar.self@gmail.com>
2019-05-31 19:43:09 -0400

    Added footer, google map capability & Smooth Scrolling

--- js/main.js
+++ js/main.js
@@ -5,3 +5,19 @@ var map;
           zoom: 8
         });
       }
+
+      // Smooth Scrolling
+      $('#navbar a, .btn').on('click', function(event){
+        if(this.hash !== '') {
+          event.preventDefault();
+
+          const hash = this.hash;
+
+          $('html, body').animate(
+            {
+              scrollTop: $(hash).offset().top - 100
+            },
+            800
+          );
+        }
+      });
